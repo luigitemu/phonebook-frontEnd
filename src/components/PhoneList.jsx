5339de6f0bf772b0f15e192e4db2178bcb9e3bf9
luigitemu <luartemu@gmail.com>
2022-01-21 10:48:35 -0600

    sort lastName

--- src/components/PhoneList.jsx
+++ src/components/PhoneList.jsx
@@ -4,27 +4,28 @@ import { useSelector } from "react-redux"
 export const PhoneList = () => {
 
     const {contacts} = useSelector(state => state.contacts); 
-
-     
     const columns = [
-        {
-            name: 'First Name',
-            selector: row => row.firstName,
-            sortable: true,
-        },
         {
             name: 'Last Name',
             selector: row => row.lastName,
-            sortable: true,
+            // sortable: true,
+        },
+        {
+            name: 'First Name',
+            selector: row => row.firstName,
+            // sortable: true,
         },
         {
             name: 'Phone Number',
             selector: row => row.phone,
         },
         {
-            cell: () => 
+            cell: ( data ) => 
             <>
-                <button className="btn btn-sm m-0 pn-btn-success me-2"> <i className="bi bi-pencil-fill"></i> </button>
+                <button 
+                className="btn btn-sm m-0 pn-btn-success me-2"
+                onClick={()=>{handleUpdate(data)}}
+                > <i className="bi bi-pencil-fill"></i> </button>
                 <button className="btn btn-sm m-0 pn-btn-danger"> <i className="bi bi-trash"></i> </button>
             </>,
             ignoreRowClick: true,
@@ -33,12 +34,25 @@ export const PhoneList = () => {
           }
     ];
 
+    const handleUpdate = (data  ) => {
+      console.log(data);
+    };
+
+    const handleDelete = (data) => {
+      console.log(data);
+    };
+    
+    
+
+
+
     return (
         <DataTable
             columns={columns}
             data={contacts}
             fixedHeader
             fixedHeaderScrollHeight="300px"
+            defaultSortFieldId={1}
         />
       );
   }
